9a7df4c358efd68d7e76c8994247091e927ec7cc
Gabriela Landberg Llave <landberg@cs.ualberta.ca>
2022-09-14 20:00:16 -0600

    Lab1 Download File and Print Contents

--- get.py
+++ get.py
@@ -2,7 +2,11 @@ import requests
 
 print(requests.get("http://www.google.com"))
 
-print(requests.get("http://raw.githubusercontent.com/AGLandberg/CMPUT404Labs/main/get.py").text)
+response = requests.get("http://raw.githubusercontent.com/AGLandberg/CMPUT404Labs/main/get.py")
+
+open("lab1file.py", "wb").write(response.content)
+
+print(response.text)
 
 
 
